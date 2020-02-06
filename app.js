0f0fa54dc080abd3be9d1aba81a8bc645162b5e5
getsomex <deathster786@gmail.com>
2020-02-06 11:24:32 +0900

    Implemented cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -23,9 +24,14 @@ app.enable('trust proxy');
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
+//  1) GLOBAL Middlewares
+// implement CORS
+app.use(cors());
+// Access-Control-Allow-Origin
+app.options('*', cors());
+// app.options('/api/v1/tour/:id', cors());
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
-//  1) GLOBAL Middlewares
 // Set security http
 app.use(helmet());
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -16,13 +16,13 @@ const createSendToken = (user, statusCode, req, res) => {
 
   res.cookie('jwt', token, {
     expires: new Date(
-      Date.now() + process.env.JWT_COOOKE_EXPIRES_IN * 24 * 60 * 60 * 100
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-
     httpOnly: true,
-    secure: req.secure || req.headers('x-forwarded-proto') === 'https'
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
   });
-  // Remove the password from the output
+
+  // Remove password from output
   user.password = undefined;
 
   res.status(statusCode).json({
